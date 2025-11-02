965a9b177644ad34874b5350795a9275ecab325a
WithFortuna <rmsghchl0@naver.com>
2025-11-02 23:45:38 +0900

    feat(service): 로또 구매 요청 처리를 위임한다

---
+++ src/main/java/lotto/service/LottoPaymentService.java
@@ -0,0 +1,15 @@
+package lotto.service;
+
+import java.util.List;
+
+import lotto.domain.Lotto;
+import lotto.controller.dto.LottoPaymentRequest;
+import lotto.controller.dto.LottoPaymentResponse;
+
+public class LottoPaymentService {
+	public LottoPaymentResponse payForLotto(LottoPaymentRequest request) {
+		List<Lotto> paidLottos = Lotto.createFromCost(request.cost());
+
+		return LottoPaymentResponse.from(paidLottos);
+	}
+}
