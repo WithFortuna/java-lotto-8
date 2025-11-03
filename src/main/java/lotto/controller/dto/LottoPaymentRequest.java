2b7e0ae33f6ae5cf81dcae911b0b772462720ee1
WithFortuna <rmsghchl0@naver.com>
2025-11-03 22:02:37 +0900

    refactor: 로또 구매관련 검증 로직을 도메인과 요청DTO 간 분리
    - LottoPaymentRequest: null이거나 empty 문자열만 검증한다
    - Lotto: 1000원 단위로 구매 가격을 입력한다는 요구사항은 도메인 검증사항으로 판단하여 Lotto 도메인에서 검증

--- src/main/java/lotto/controller/dto/LottoPaymentRequest.java
+++ src/main/java/lotto/controller/dto/LottoPaymentRequest.java
@@ -1,15 +1,20 @@
 package lotto.controller.dto;
 
 public record LottoPaymentRequest(
-	Integer cost
+	Integer lottoCost
 ) {
-	public LottoPaymentRequest {
-		if (cost % 1000 != 0) {
-			throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위여야합니다");
+	public static LottoPaymentRequest from(String cost) {
+		validateNotEmpty(cost);
+		try {
+			return new LottoPaymentRequest(Integer.parseInt(cost));
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
 		}
 	}
 
-	public static LottoPaymentRequest from(String cost) {
-		return new LottoPaymentRequest(Integer.parseInt(cost));
+	private static void validateNotEmpty(String cost) {
+		if (cost == null || cost.isEmpty()) {
+			throw new IllegalArgumentException("[ERROR] 구입 금액을 반드시 입력해주세요");
+		}
 	}
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,29 +10,41 @@ public class Lotto {
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
-		validate(numbers);
+		validateLottoNumbers(numbers);
 		this.numbers = numbers;
 	}
 
 	public static List<Lotto> createFromCost(int cost) {
+		validateLottoCost(cost);
+
 		int theNumberOfLotto = cost / 1000;
-
 		List<Lotto> lottos = new ArrayList<>();
-
 		for (int i = 0; i < theNumberOfLotto; i++) {
-			TreeSet<Integer> numbers = new TreeSet<>();
-			while (numbers.size() < 6) {
-				numbers.add(Randoms.pickNumberInRange(1, 45));
-			}
-			lottos.add(new Lotto(List.copyOf(numbers)));
+			Lotto lotto = createLotto(lottos);
+			lottos.add(lotto);
 		}
 
 		return lottos;
 	}
 
-	private void validate(List<Integer> numbers) {
+	private static Lotto createLotto(List<Lotto> lottos) {
+		TreeSet<Integer> numbers = new TreeSet<>();
+		while (numbers.size() < 6) {
+			numbers.add(Randoms.pickNumberInRange(1, 45));
+		}
+		return new Lotto(List.copyOf(numbers));
+	}
+
+	private static void validateLottoCost(int cost) {
+		if (cost % 1000 != 0) {
+			throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위여야합니다");
+		}
+	}
+
+	private void validateLottoNumbers(List<Integer> numbers) {
 		checkLottoSize(numbers);
 		checkLottoElements(numbers);
+		checkDuplication(numbers);
 	}
 
 	private static void checkLottoElements(List<Integer> numbers) {
@@ -50,6 +62,13 @@ public class Lotto {
 	private static void checkLottoSize(List<Integer> numbers) {
 		if (numbers.size() != 6) {
 			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+	}
+
+	private static void checkDuplication(List<Integer> numbers) {
+		long distinctCount = numbers.stream().distinct().count();
+		if (distinctCount != numbers.size()) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
 		}
 	}
 
