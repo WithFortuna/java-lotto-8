25767700f260e6f89c13ee2dfd5c0ae3e406fdba
WithFortuna <rmsghchl0@naver.com>
2025-11-03 21:48:41 +0900

    feat(view): 로또 당첨 번호를 콘솔로부터 입력받는다

--- src/main/java/lotto/view/input/ConsoleUserInput.java
+++ src/main/java/lotto/view/input/ConsoleUserInput.java
@@ -3,10 +3,26 @@ package lotto.view.input;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleUserInput{
-	public String payForLotto() {
+	public String inputLottoCost() {
 		System.out.println("구입금액을 입력해 주세요.");
 		String cost = Console.readLine();
 
 		return cost;
+	}
+
+	public String inputWinningNumbers() {
+		System.out.println();
+		System.out.println("당첨 번호를 입력해 주세요.");
+		String winningNumbers = Console.readLine();
+
+		return winningNumbers;
+	}
+
+	public String inputBonusNumber() {
+		System.out.println();
+		System.out.println("보너스 번호를 입력해 주세요.");
+		String bonusNumber = Console.readLine();
+
+		return bonusNumber;
 	}
 }
